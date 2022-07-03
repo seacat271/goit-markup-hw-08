1d27cc197be46e6ac6b5dc96d9bda839c5c928a6
seacat271 <seacat271083@gmail.com>
2022-07-03 18:18:04 +0300

    backgroud-img

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,6 +2,7 @@
 const menuBtnRef = document.querySelector("[data-menu-button]");
 const mobileMenuRef = document.querySelector("[data-menu]");
 
+
 menuBtnRef.addEventListener("click", () => {
     const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -9,6 +10,8 @@ menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
+
 });
+
 
 })();
